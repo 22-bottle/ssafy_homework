40d0a519608b13b1515fb69bf52f16bb604a1f6f
22-bottle <gmlquddl67@naver.com>
2023-09-04 21:30:10 +0900

    0904

---
+++ hw11/js/cafe.js
@@ -0,0 +1,13 @@
+function login() {
+    let id = prompt("아이디 입력");
+    let pw = prompt("비밀번호 입력");
+    if (id == "ssafy" && pw == "1234") {
+        alert("로그인 성공!");
+    } else {
+        alert("로그인 실패");
+    }
+    
+}
+
+
+
